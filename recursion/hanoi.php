01fb8550da42e23750103705f08428e5d8ed6ba7
r-ume <baymax.blackandwhite@gmail.com>
2017-05-15 14:28:16 +0000

    modified hanoi.php

--- recursion/hanoi.php
+++ recursion/hanoi.php
@@ -11,7 +11,7 @@ hanoi(3, "A", "B", "C");
 function hanoi($n, $a, $b, $c){
 	if($n > 0){
 		hanoi($n-1, $a, $b, $c);
-		echo 'moving'. $n . 'th hanoi' . ' from'. $a .'to' . $b;
+		echo "moving $n th hanoi from $a to $b";
 		echo "\n";
 		hanoi($n-1, $a, $b, $c);
 	}
