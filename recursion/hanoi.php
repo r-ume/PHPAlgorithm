ef557384fc56601ad676efe103c7a59e47d839f3
r-ume <baymax.blackandwhite@gmail.com>
2017-05-15 14:20:15 +0000

    solved hanoi issue

---
+++ recursion/hanoi.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "hanoi 2 \n";
+hanoi(2, "A", "B", "C");
+
+echo "\n";
+
+echo "hanoi 3 \n";
+hanoi(3, "A", "B", "C");
+
+function hanoi($n, $a, $b, $c){
+	if($n > 0){
+		hanoi($n-1, $a, $b, $c);
+		echo 'moving'. $n . 'th hanoi' . ' from'. $a .'to' . $b;
+		echo "\n";
+		hanoi($n-1, $a, $b, $c);
+	}
+}
