ce2998c7a238ff8d61f3d80658441490a5b2eca9
児玉悠磨 <yumakodama@kodamayuumanoMacBook-Pro.local>
2017-06-20 14:02:48 +0900

    自学としてtechでの例題を追加しました。

--- yuma_practice/hoge.php
+++ yuma_practice/hoge.php
@@ -1,3 +1,20 @@
 <?php
 
-echo "hoge";
+$post = array();
+$post["genre"] = "漫画";
+$post["title"] = "帝一の國";
+$post["review"] = "良い";
+
+var_dump($post)
+
+
+ // array(3) {
+　　["genre"]=>
+　　string(9) "マンガ"
+　　["title"]=>
+　　string(18) "るろうに剣心"
+　　["review"]=>
+　　string(10) "面白い!"
+　}
+
+
