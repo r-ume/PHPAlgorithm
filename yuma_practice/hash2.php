b6d736079b133c22069b00a5a12104f59f1e66fe
r-ume <baymax.blackandwhite@gmail.com>
2017-06-23 01:22:38 +0000

    別の方法での連想配列の書き方を書いた

--- yuma_practice/hash2.php
+++ yuma_practice/hash2.php
@@ -5,4 +5,11 @@ $waseda["department"]["bunkou"] = 'yuma';
 
 $waseda["department"]["sils"] = 'ryo';
 
+$waseda2 = [];
+$waseda2 = [
+	"department" => [
+		"jinka" => "rei"
+	]
+];
 
+var_dump($waseda2);
