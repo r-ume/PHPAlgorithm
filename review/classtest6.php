847439495b2075db12c68b3e06a55fd3d69448e6
r-ume <baymax.blackandwhite@gmail.com>
2018-02-25 11:33:31 +0900

    added Media class

--- review/classtest6.php
+++ review/classtest6.php
@@ -1,19 +1,35 @@
 <?php
 
-class Television{
-    private $channelNo;
+class Media{
 
-    // コンストラクタの書き方
-    function _construct(){
-        $this->channelNo = 8; // Javaの　this.変数＝　みたいな書き方
-    }
+  private $type;
 
-    function getChannel(){
-        return $this->channelNo;
-    }
+  public function __construct($type){
+    $this->type = $type;
+  }
+
+  public function getType(){
+    return $this->type;
+  }
+}
+
+class Television extends Media{
+  private $channelNo;
+  private $type;
+
+  // コンストラクタの書き方
+  function __construct($type){
+    parent::__construct($type);
+    $this->type = $type;
+    $this->channelNo = 8; // Javaの　this.変数＝　みたいな書き方
+  }
+
+  function getChannel(){
+    return $this->channelNo;
+  }
 }
 
 // コンストラクタの書き方
-$tvl = new Television();
-print('the current channel'.$tvl -> getChannel().'<br>');
+$tvl = new Television('Television');
+print('the current channel'.$tvl->getType().'<br>');
      
